21a835567a29b322032f0200fed6ff1e1d82deaf
slond0 <137227033+slond0@users.noreply.github.com>
2026-02-26 07:21:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,19 @@ st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('img.png')
 
 st.image(image, caption='Interfaces multimodales')
+
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
+
+st.subheader("Ahora usemos 2 Columnas")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Esta es la primera columna")
+    st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+    resp = st.checkbox('Estoy de acuerdo')
+    if resp:
+       st.write('Correcto!')
+
