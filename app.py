55aa3ebe1abb0f06745e44b85c58235b9cfbcc63
slond0 <137227033+slond0@users.noreply.github.com>
2026-02-26 07:08:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from PIL import Image
 
-st.title("PRUEBA APAGINA CON STREAM LIT")
+st.title("Glam page")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('Interfaces Mult2.png')
